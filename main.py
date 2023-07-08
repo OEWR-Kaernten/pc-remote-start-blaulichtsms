7fefb9ef281a81bc432a48bea3cc563398570d84
Peddaahh <peddaahh@gmail.com>
2023-07-08 13:06:28 +0200

    addedpyinstaller

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import uvicorn
 from decouple import config
+from multiprocessing import freeze_support
 
 if __name__ == "__main__":
+    freeze_support()
     root_path = config("ROOT_PATH", default="")
     uvicorn.run("src.api:app", host="0.0.0.0", port=8080, reload=True, root_path=root_path)
